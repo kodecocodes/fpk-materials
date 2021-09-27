c9a6d38c704297cb6631487c33ff31b3fd82f9ee
Martyn Haigh <martyn.haigh@gmail.com>
2021-09-27 20:24:10 +0100

    TE first pass

--- 06-immutability-and-recursion/projects/final/src/main/kotlin/com/raywenderlich/fp/FPImmutability.kt
+++ 06-immutability-and-recursion/projects/final/src/main/kotlin/com/raywenderlich/fp/FPImmutability.kt
@@ -39,14 +39,14 @@ fun main() {
       total += list[i]
     }
   }
-  println("Total $total")
+  println("Total: $total")
    */
   /*
   val multipleOf5 = { value: Int -> value % 5 == 0 }
   val total = listOf(1, 5, 10, 12, 34, 55, 80, 23, 35, 12, 80)
     .filter(multipleOf5)
     .sum()
-  println("Total $total")
+  println("Total: $total")
    */
 
   // DON'T DO THIS!!!
@@ -57,5 +57,5 @@ fun main() {
         total += it
       }
     }
-  println("Total $total")
+  println("Total: $total")
 }
